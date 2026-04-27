8c22e554a2c03226c6f3744914735467380aa2ba
meehakimov <meehakimov@gmail.com>
2026-04-27 18:42:32 +0500

    attemp 4

--- assets/js/index.js
+++ assets/js/index.js
@@ -95,21 +95,3 @@ hamburger.addEventListener('click', () => {
     isOpen = false;
   }
 });
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBLDmOwRHWLNvp2QOrBaRg1Deiwk33sd30",
-  authDomain: "notiport-7c96a.firebaseapp.com",
-  projectId: "notiport-7c96a",
-  storageBucket: "notiport-7c96a.firebasestorage.app",
-  messagingSenderId: "658354889549",
-  appId: "1:658354889549:web:4147f82bf6dfa996570d04",
-  measurementId: "G-DT7XFB510W"
-};
- 
